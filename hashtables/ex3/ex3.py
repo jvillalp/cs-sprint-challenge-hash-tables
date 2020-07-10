9c22534d321b7bf560805630064665c7f8e6803d
Juana Villalpando <juana.v14@gmail.com>
2020-07-10 09:16:29 -0700

    example 3 complete

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,9 +2,23 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
+    num_lists = len(arrays)
+    seen = {}
+    result = []
+    for i in range(num_lists):
+        for j in range(len(arrays[i])):
+            if arrays[i][j] in seen:
+                seen[arrays[i][j]].append(i)
+            else:
+                seen[arrays[i][j]] = [i]
+    
+    for key in seen:
+        if len(seen[key]) == num_lists:
+            result.append(key)
+    #return list of all integers in lists that are the same
     return result
+
+
 
 
 if __name__ == "__main__":
